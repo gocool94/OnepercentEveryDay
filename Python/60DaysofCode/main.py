3a7db6d98467d4b5cff6d72eb8ee2ee58c7dcb62
gocool94 <gocool94@gmail.com>
2023-10-04 14:18:27 +0530

    0/50

--- Python/60DaysofCode/main.py
+++ Python/60DaysofCode/main.py
@@ -1,1 +1,8 @@
-user_text = input("Enter a todo: ")
+
+
+todos = []
+print("tttt".title())
+while True:
+    user_text = input("Enter a todo: ").capitalize()
+    todos.append(user_text)
+    print(todos)
